ed76cdeb1721f7f80a1c6508999f7c4cbc65597d
Amlogic_Public <Amlogic_Public@protonmail.com>
2025-12-25 19:26:19 +0800

    c

--- cpp/include/shared/SharedData.hpp
+++ cpp/include/shared/SharedData.hpp
@@ -1,42 +1,31 @@
 #pragma once
 
-struct Config;
-struct Asset;
-struct AssetInfo;
-struct Feature;
-struct Dist;
-struct TimeSeries;
-struct TaskState;
-struct TaskTerminal;
-struct CoroManager;
-
 #include "./Asset.hpp"
 #include "./AssetInfo.hpp"
 #include "./Config.hpp"
-#include "./OrderFlow.hpp"
-#include "./Feature.hpp"
 #include "./Dist.hpp"
-#include "./TimeSeries.hpp"
+#include "./Feature.hpp"
+#include "./OrderFlow.hpp"
 #include "./TaskState.hpp"
-#include "gui/task_terminal/TaskTerminal.hpp"
+#include "./TimeSeries.hpp"
 #include "gui/coro/CoroManager.hpp"
+#include "gui/task_terminal/TaskTerminal.hpp"
+
 
 struct SharedData {
   Config config;
-  Asset asset;           // L2 database metadata
-  AssetInfo assetinfo;  // Stock info from Baostock
-  TaskTerminal terminal;
-  CoroManager coromgr;
-  OrderFlow orderflow;
+  TaskState taskstate;
+  Asset asset;
+  AssetInfo assetinfo;
   Feature feature;
+  OrderFlow orderflow;
   Dist dist;
   TimeSeries timeseries;
-  TaskState taskstate;  // 统一的任务状态管理
-  
-  // Request full GUI reinitialization (triggered by config sync)
+
+  CoroManager coromgr;
+  TaskTerminal terminal;
+
   bool request_reinit = false;
-  
-  // High Performance Mode: GUI thread sleeps, all CPU for compute tasks
   bool high_performance_mode = false;
   void EnableHighPerformanceMode() { high_performance_mode = true; }
   void DisableHighPerformanceMode() { high_performance_mode = false; }
